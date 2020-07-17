e4b5ee54c16dc61ac04e47887eae3597710bedf8
Pichelbauer1810640038 <48803701+Pichelbauer1810640038@users.noreply.github.com>
2020-07-17 20:58:02 +0200

    Update App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -112,7 +112,7 @@ class App extends Component {
     Meteor.subscribe('tasks');
 
     return {
-      tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+      tasks: Tasks.find({}, { sort: { text: 1 } }).fetch(),
       incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
       currentUser: Meteor.user(),
     };
